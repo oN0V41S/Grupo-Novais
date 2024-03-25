65dc444a4dc151560d4c0828b7c2c85fbf63d95f
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2024-03-25 00:20:08 +0000

    Adicionando recursos na página de registro

--- src/pages/registerProduct/registerProduct.jsx
+++ src/pages/registerProduct/registerProduct.jsx
@@ -1,14 +1,40 @@
 import '@pages/registerProduct/registerProduct.scss'
 
 export default function RegisterProduct(){
+  
+  
   return (
-    <main class="registerProduct">
+    <main className="registerProduct">
     <h1>Registre um Produto</h1>
     <form>
-      <input name="name" type="text" placeholder="Nome do Produto"/>
-      <input name="price" type="number" placeholder="Preço"/>
-      <input name="imgLink" type="text" placeholder="Link da imagem"/>
+      <input name="name" id="nome" type="text" placeholder="Nome do Produto"/>
+      <input name="price" id="preco" type="number" placeholder="Preço"/>
+      <input name="imgLink" id="img" type="text" placeholder="Link da imagem"/>
+      <select>
+        <option>Tênis</option>
+        <option>Roupa</option>
+        <option>Tecnologia</option>
+      </select>      
+      <button type="button" onClick={cadastrarProduto}>Cadastrar</button>
     </form>
     </main>
   )
+}
+
+const cadastrarProduto = () => {
+    let url = 'https://api.sheety.co/83adb88178c8983cdefdf18d642e9e55/fabricaDeOutfit/sneakers';
+    let body = {
+    sneaker: {
+      
+    }
+  }
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body)
+  })
+  .then((response) => response.json())
+  .then(json => {
+    // Do something with object
+    console.log(json.sneaker);
+  });
 }
