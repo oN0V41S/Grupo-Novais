699f4eea9e8d53d7081678272f305b2c6dc245df
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2024-04-14 15:47:54 +0000

    fix: product model parameters

--- server/src/model/product.js
+++ server/src/model/product.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 // Modelo do objeto de Produto que será utilizado no banco
 const Product = mongoose.model('Film', {
-  name:        String,
-  price:  Double,
-  link_buy: String,
-  images:    [String]
+  name:         String,
+  preco:        String,
+  link_buy:     String,
+  images:       [String]
 });
 
 module.exports = Product;
---
+++ server/src/model/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+// Modelo do objeto de Usuário
+const User = mongoose.model('User',{
+  name: String,
+  email: String,
+  password: String,
+  itens_saved: [String],
+  itens_card: [String]
+})
