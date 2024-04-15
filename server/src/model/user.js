29941e0db814913098c60dd8fb6a6dbaba2a4398
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2024-04-15 13:38:57 +0000

    fix: export User model

--- server/src/model/user.js
+++ server/src/model/user.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Modelo do objeto de Usuário
-const User = mongoose.model('User',{
+const User = mongoose.model("User", {
   name: String,
   email: String,
   password: String,
   itens_saved: [String],
-  itens_card: [String]
-})
+  itens_card: [String],
+});
+
+module.exports = User;
